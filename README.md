6899ae6d74f2195bf8d988d062991ca194448ec5
asiryk <asiryk.net@gmail.com>
2021-12-26 20:59:38 +0200

    Normalize text

---
+++ README.md
@@ -0,0 +1,2 @@
+Assingment from here
+https://github.com/deskool/nlp-class/blob/master/homework/HW1/assignment.ipynb
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,38 @@
-mod m0_str;
+#[macro_use]
+extern crate lazy_static;
 
-use m0_str::handle_string;
+use regex::Regex;
 use std::fs::read_to_string;
 
+lazy_static! {
+    static ref WHITESPACE: Regex = Regex::new(r"[ \n\r\t]+").unwrap(); // find all whitespaces
+}
+
+const FILE_PATH: &str = "assets/beyond-good-and-evil.txt";
+
 fn main() {
-    let str_file = read_to_string("assets/the-problems-of-philosophy.txt").unwrap();
-    handle_string(str_file);
+    let mut text = read_to_string(FILE_PATH).unwrap();
+    let text = normalize(&mut text);
+    println!("{}", text.len());
+}
+
+fn normalize(text: &mut String) -> String {
+    WHITESPACE.replace_all(text, " ").to_lowercase()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::normalize;
+
+    #[test]
+    fn replace_whitespaces_and_lowercase() {
+        let mut text = String::from(
+            "SOME
+            tExt  \n\r\r\r\t\t  THat wiLL
+    be normalized",
+        );
+        let result = String::from("some text that will be normalized");
+
+        assert_eq!(result, normalize(&mut text));
+    }
 }
