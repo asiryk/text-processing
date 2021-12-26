ded8f471651d6128f37ef2ac135fbecf453ea8d7
asiryk <asiryk.net@gmail.com>
2021-12-26 19:24:33 +0200

    Basic string staff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+mod m0_str;
+
+use m0_str::handle_string;
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, world!");
+    let str_file = read_to_string("assets/the-problems-of-philosophy.txt").unwrap();
+    handle_string(str_file);
 }
